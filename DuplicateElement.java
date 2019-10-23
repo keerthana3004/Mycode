1011fdb92c5dc98eb42326f2d9154f56f65f6c17
keerthana3004 <56907648+keerthana3004@users.noreply.github.com>
2019-10-23 18:39:52 +0530

    Update DuplicateElement.java

--- DuplicateElement.java
+++ DuplicateElement.java
@@ -1,3 +1,5 @@
+/*created by keerthana
+To find the duplicate number*/
 package duplicateelement;
 
 public class DuplicateElement {
@@ -6,9 +8,9 @@ public class DuplicateElement {
         int[] arr = {2, 3, 2, 7, 6, 8};
         for (int i = 0; i < arr.length-1; i++)
         {
-            for (int j = i + 1; j < arr.length-1; j++)
+            for (int j = i + 1; j < arr.length-1; j++)/*j=j+1 it takes the next value of i */
             {
-                if (arr[i] == arr[j])
+                if (arr[i] == arr[j])/*if the value of the arr[i] is same as arr[j]value it will execute the duplicate element*/
                 {
                     System.out.println("duplicate element is:" + arr[j]);
                 }
